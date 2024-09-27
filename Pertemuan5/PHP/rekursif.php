d73c70dac5011f8d816306c0b37265043d72a32d
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-09-27 07:44:14 +0700

    pertemuan5_php_soal6(2)

--- Pertemuan5/PHP/rekursif.php
+++ Pertemuan5/PHP/rekursif.php
@@ -6,6 +6,20 @@ function tampilkanHaloDunia(){
     tampilkanHaloDunia();
 }
 
-tampilkanHaloDunia();
+//tampilkanHaloDunia();
 
+for ($i=1; $i <= 25 ; $i++) { 
+    echo "Perulangan ke-{$i} <br>";
+}
+
+function tampilkanAngka (int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //Panggil diri sendiri selama $indeks <= jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+echo "<br>";
+tampilkanAngka(20);
 ?>
