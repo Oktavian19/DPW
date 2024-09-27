5abd7dbaef58a8cd3e37b91715d9bd93f1db451c
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-09-27 08:27:25 +0700

    pertemuan5_php_soal8

--- Pertemuan5/PHP/array.php
+++ Pertemuan5/PHP/array.php
@@ -38,6 +38,9 @@ $menu = [
       echo "<ul>";
       foreach ($menu as $key => $item) {
         echo "<li>{$item['nama']}</li>";
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
       }
       echo "</ul>";
     }
