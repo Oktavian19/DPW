a4bd7712275a7c58cbe7e6ea3b9e053dd1abc84a
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-09-27 07:21:50 +0700

    pertemuan5_php_soal1

---
+++ Pertemuan5/PHP/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Oktavian Eka Ramadhan<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//Memanggil fungsi yang sudah dibuat
+perkenalan();
+
+?>
