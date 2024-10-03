3b7eda851a0a009a1ab72002ea9c2b94eb93cfac
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-10-03 21:11:34 +0700

    pertemuan6_soal4

---
+++ Pertemuan6/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalan, nama saya Oktavian Eka Ramadhan<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+
+perkenalan();
+?>
