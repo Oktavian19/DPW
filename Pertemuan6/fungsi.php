33ca9893481278a0085ae7bf2ae493d8fcfe357c
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-10-04 07:24:24 +0700

    pertemuan6_soal8

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -1,13 +1,15 @@
 <?php
-function perkenalan($nama, $salam="Assalamualaikkum") {
-    echo "$salam, ";
-    echo "Perkenalan, nama saya $nama<br>";
-    echo "Senang berkenalan dengan Anda<br>";
-}
-
 function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
+}
+
+function perkenalan($nama, $salam="Assalamualaikkum") {
+    echo "$salam, ";
+    echo "Perkenalan, nama saya $nama<br>";
+
+    echo "Saya berusia ". hitungUmur(2004, 2023) . " tahun<br/>";
+    echo "Senang berkenalan dengan Anda<br>";
 }
 
 echo "Umur saya adalah ". hitungUmur(2004, 2023) ." tahun";
