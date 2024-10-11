75e0207a83938e83dceab36a59a937c717bd9dde
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-10-11 08:36:46 +0700

    pertemuan7_soal4.2

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -9,4 +9,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo "Request tidak valid.";
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email : ". $email;
+    } else {
+        echo "Format email tidak valid";
+    }
+} else {
+    echo "Tidak ada email yang dimasukkan";
+}
 ?>
