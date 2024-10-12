4532d4575b46e920f354dbfdefe9e54a0d11b059
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-10-12 13:03:16 +0700

    pertemuan7_soal7.1

---
+++ Pertemuan7/form_validasi.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Input dengna Validasi</title>
+</head>
+<body>
+    <h1>Form Input dengan Validasi</h1>
+    <form action="proses_validasi.php" method="post">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama">
+        <br>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
