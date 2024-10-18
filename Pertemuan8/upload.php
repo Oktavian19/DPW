86da5ce4dea2cf38e1ea995c541729c4a376c4c7
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-10-18 09:19:01 +0700

    pertemuan8_soal12

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -1,14 +1,14 @@
 <?php
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $targetfile = $targetdir . basename($_FILES["file"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
     $maxsize = 3 * 1024 * 1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+    if (in_array($fileType, $allowedExtensions) && $_FILES["file"]["size"] <= $maxsize) {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah<br><br>";
             echo "<img src='" . $targetfile . "' width='200px'>";
         } else {
