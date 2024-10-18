8df03b453235794bfb9a2c3dbd5c296208dc0568
Oktavian Eka Ramadhan <72781555+Oktavian19@users.noreply.github.com>
2024-10-18 07:45:24 +0700

    pertemuan8_soal5

--- Pertemuan8/upload.php
+++ Pertemuan8/upload.php
@@ -9,7 +9,8 @@ if (isset($_POST["submit"])) {
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah";
+            echo "File berhasil diunggah<br><br>";
+            echo "<img src='" . $targetfile . "' width='200px'>";
         } else {
             echo "Gagal mengunggah file";
         }
